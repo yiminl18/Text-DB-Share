e7c7a78439f7686c810aa44d42bcf70293f6cf78
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2025-01-30 19:35:27 -0800

    add json output

--- models/gpt_4o.py
+++ models/gpt_4o.py
@@ -10,8 +10,9 @@ openai.api_key = api_key
 
 #print(api_key)
 
-def chatGPT_api(message_content,temperature=0):
+def chatGPT_api(message_content,json,temperature=0):
     ##message_content is string
+    
     response = client.chat.completions.create(model = "gpt-4o",
         messages = [
             {"role": "user", "content": message_content}],
@@ -20,7 +21,7 @@ def chatGPT_api(message_content,temperature=0):
     return response.choices[0].message.content
     #return response["choices"][0]["message"]["content"]
 
-def gpt_4o(prompt):
+def gpt_4o(prompt,json):
     message_content = prompt[0] + prompt[1]
-    return chatGPT_api(message_content)
+    return chatGPT_api(message_content,json)
 
