e40c9f664cda205f3917b23748b07dc413b9ca8b
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2025-02-01 12:01:52 -0800

    add llama3.2

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 
 #this is the models API. You pass the model (name of the model) and prompt, the API will return the response out 
-def model(model_name, prompt):
+def model(model_name, prompt, json = 0):
     if(model_name is 'gpt35'):
         from models.gpt_35_model import gpt_35
         return gpt_35(prompt)
@@ -9,7 +9,13 @@ def model(model_name, prompt):
         return gpt_4(prompt)
     if(model_name == 'gpt4o'):
         from models.gpt_4o import gpt_4o
-        return gpt_4o(prompt)
+        return gpt_4o(prompt, json)
+    if(model_name == 'deepseek_1.5'):
+        from models.deepseek_1_5 import deepseek_1_5
+        return deepseek_1_5(prompt)
+    if(model_name == 'llama3.2'):
+        from models.llama_3_2 import llama_3_2
+        return llama_3_2(prompt)
     return 'input model does not exist'
 
 
